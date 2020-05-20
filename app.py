9b1725824b0846176fdc13e79e84a2923e527fc1
Kulwinder Singh <kulwinder_31@hotmail.com>
2020-05-20 10:13:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from blogsite import app
+import subprocess
+
+command = 'sudo apt-get install python-flask'
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
 
 
 if __name__ == '__main__':
