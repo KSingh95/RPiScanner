945cc813d4ec1e244b5295727c68099edc8015c4
Kulwinder Singh <kulwinder_31@hotmail.com>
2020-05-20 10:25:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 from blogsite import app
-import subprocess
-
-command = 'sudo apt-get install python-flask'
-process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
