8b546976d83e81a3af534911f10278714a38ff83
Kulwinder Singh <kulwinder_31@hotmail.com>
2020-05-20 10:24:57 -0400

    Update __init__.py

--- blogsite/__init__.py
+++ blogsite/__init__.py
@@ -1,11 +1,11 @@
-#from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-#app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'c0d7a9a3401a772357254f4f9098bdaa'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1995@localhost:5000/rpidata'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
-#db = SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c0d7a9a3401a772357254f4f9098bdaa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1995@localhost:5000/rpidata'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+db = SQLAlchemy(app)
 
 from blogsite import routes
